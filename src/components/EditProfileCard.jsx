04a8dfc5debc00f00bdf7ed64efab286e35557dd
sonideepak08 <100990869+sonideepak08@users.noreply.github.com>
2026-05-17 21:13:04 +0530

    some refactoring

--- src/components/EditProfileCard.jsx
+++ src/components/EditProfileCard.jsx
@@ -39,8 +39,8 @@ const EditProfileCard = ({ user }) => {
 
   return (
     <>
-      <div className="flex justify-center my-10">
-        <div className="flex justify-center mx-10">
+      <div className="flex justify-center items-start gap-10 my-10 min-h-screen">
+        <div>
           <div className="card bg-base-300 w-96 shadow-xl">
             <div className="card-body">
               <h2 className="card-title justify-center">Edit Profile</h2>
